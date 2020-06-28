2489f6cbd7fc148743e84e5985e49ffa93e0f27d
MecaVol <landry@mecavol.com>
2020-06-28 17:38:30 +0400

    Documentation parser WIP

--- builder/service_builder.dart
+++ builder/service_builder.dart
@@ -1,6 +1,6 @@
 import 'dart:io' show File;
 import 'package:mustache_template/mustache_template.dart';
-import 'proto/krpc.pb.dart' show Service, Procedure;
+import 'proto/krpc.pb.dart' show Service, Procedure, Parameter;
 import 'utils/string_utils.dart';
 import 'utils/type_utils.dart';
 
@@ -34,6 +34,8 @@ class ServiceBuilder {
 
     _buildClasses();
     _buildEnumerations();
+    _buildExceptions();
+    _buildProcedures();
 
     outFile.writeAsStringSync(_template.renderString(data));
   }
@@ -71,129 +73,112 @@ class ServiceBuilder {
       data['enumerations'].add(enumData);
     });
   }
-}
 
-/// Builds the library root file, the one that needs to be imported to use it.
-void buildService(Service service) {
-  print('\nBuilding ${service.name}...');
-
-  var template =
-      Template(File('builder/templates/services.html').readAsStringSync());
-
-  var fileName = 'lib/src/services/${toSnakeCase(service.name)}.dart';
-  File(fileName).createSync(recursive: true);
-  var file = File(fileName);
-
-  var procedure_data = parseProcedures(service.procedures);
-
-  var serviceProcedures = procedure_data['service_procedures'];
-  var serviceGetters = procedure_data['service_getters'];
-  var serviceSetters = procedure_data['service_setters'];
-  var classMethods = procedure_data['class_methods'];
-  var classStaticMethods = procedure_data['class_static_methods'];
-  var classGetters = procedure_data['class_getters'];
-  var classSetters = procedure_data['class_setters'];
-
-  var classes = [];
-  service.classes.forEach((item) {
-    var thisClassMethods = classMethods.where((element) => element['class'] == item.name).toList();
-    classes.add({
-      'name': item.name,
-      'documentation': parseDoc(item.documentation),
-      'class_methods': thisClassMethods,
+  void _buildExceptions() {
+    // To remove duplicates:
+    var exceptionsList = _service.exceptions.toSet().toList();
+    // Then build:
+    exceptionsList.forEach((exception) {
+      data['exceptions'].add({
+        'exception_name': toPascalCase(exception.name),
+        'documentation': parseDoc(exception.documentation),
+      });
     });
-  });
+  }
 
-  var procedures = [];
-  service.procedures.forEach((procedure) {
-    var parameters = [];
-    procedure.parameters.forEach((parameter) {
-      parameters.add({
-        'name':
-            parameter.name == 'this' ? '_' + parameter.name : parameter.name,
-        'type': convert(parameter.type),
+  void _buildProcedures() {
+    var filteredProcedures = _parseProcedures(_service.procedures);
+    var serviceProcedures = filteredProcedures['service_procedures'];
+    serviceProcedures.forEach((procedureData) {
+      procedureData['procedure_name'] =
+          toCamelCase(procedureData['procedure_name']);
+      Procedure procedure = procedureData['procedure'];
+      procedureData['return_type'] = convert(procedure.returnType);
+      procedureData['parameters'] = _buildParameters(procedure.parameters);
+      procedureData['documentation'] = parseDoc(procedure.documentation);
+      procedureData['raw_doc'] = procedure.documentation;
+    });
+    data['service_procedures'] = serviceProcedures;
+  }
+
+  static Map<String, dynamic> _parseProcedures(List<Procedure> procedures) {
+
+    var result = {
+      'service_procedures': [],
+      'service_getters': [],
+      'service_setters': [],
+      'class_methods': [],
+      'class_static_methods': [],
+      'class_getters': [],
+      'class_setters': [],
+    };
+
+    var serviceProcedureRE = RegExp(r'^[A-Za-z]+$');
+    var serviceGetterProcedureRE = RegExp(r'^get_([A-Za-z]+)$');
+    var serviceSetterProcedureRE = RegExp(r'^set_([A-Za-z]+)$');
+    var classMethodProcedureRE = RegExp(r'^([A-Za-z]+)_([A-Za-z]+)$');
+    var classStaticMethodProcedureRE =
+    RegExp(r'^([A-Za-z]+)_static_([A-Za-z]+)$');
+    var classGetterMethodProcedureRE = RegExp(r'^([A-Za-z]+)_get_([A-Za-z]+)$');
+    var classSetterMethodProcedureRE = RegExp(r'^([A-Za-z]+)_set_([A-Za-z]+)$');
+
+
+    procedures.forEach((procedure) {
+
+      if (serviceProcedureRE.hasMatch(procedure.name)) {
+        print('  ${procedure.name}');
+        result['service_procedures'].add({'procedure': procedure, 'procedure_name': procedure.name});
+
+      } else if (serviceGetterProcedureRE.hasMatch(procedure.name)) {
+        var match = serviceGetterProcedureRE.firstMatch(procedure.name);
+        print('  ${match.group(1)} [get]');
+        result['service_getters'].add({'procedure': procedure, 'procedure_name': match.group(1)});
+
+      } else if (serviceSetterProcedureRE.hasMatch(procedure.name)) {
+        var match = serviceSetterProcedureRE.firstMatch(procedure.name);
+        print('  ${match.group(1)} [set]');
+        result['service_setters'].add({'procedure': procedure, 'procedure_name': match.group(1)});
+
+      } else if (classMethodProcedureRE.hasMatch(procedure.name)) {
+        var match = classMethodProcedureRE.firstMatch(procedure.name);
+        print('  ${match.group(1)} - ${match.group(2)}');
+        result['class_methods']
+            .add({'class_name': match.group(1), 'procedure': procedure, 'procedure_name': match.group(2)});
+
+      } else if (classStaticMethodProcedureRE.hasMatch(procedure.name)) {
+        var match = classStaticMethodProcedureRE.firstMatch(procedure.name);
+        print('  ${match.group(1)} - ${match.group(2)} [static]');
+        result['class_static_methods']
+            .add({'class_name': match.group(1), 'procedure': procedure, 'procedure_name': match.group(2)});
+
+      } else if (classGetterMethodProcedureRE.hasMatch(procedure.name)) {
+        var match = classGetterMethodProcedureRE.firstMatch(procedure.name);
+        print('  ${match.group(1)} - ${match.group(2)} [get]');
+        result['class_getters']
+            .add({'class_name': match.group(1), 'procedure': procedure, 'procedure_name': match.group(2)});
+
+      } else if (classSetterMethodProcedureRE.hasMatch(procedure.name)) {
+        var match = classSetterMethodProcedureRE.firstMatch(procedure.name);
+        print('  ${match.group(1)} - ${match.group(2)} [set]');
+        result['class_setters']
+            .add({'class_name': match.group(1), 'procedure': procedure, 'procedure_name': match.group(2)});
+      }
+    });
+    return result;
+  }
+
+  static List<Map<String, dynamic>> _buildParameters(List<Parameter> parameters) {
+    var result = <Map<String, dynamic>>[];
+    parameters.forEach((parameter) {
+      result.add({
+        'parameter_name': toCamelCase(parameter.name),
+        'parameter_type': convert(parameter.type),
+        'parameter_default_value': parameter.defaultValue.toString(),
         'comma': true,
       });
     });
-    if (parameters.isNotEmpty) parameters.last['comma'] = false;
-
-    procedures.add({
-      'name': toCamelCase(procedure.name),
-      'parameters': parameters,
-      'return_type': convert(procedure.returnType),
-    });
-  });
-
-  file.writeAsStringSync(template.renderString({
-    'documentation': parseDoc(service.documentation),
-    'name': toPascalCase(service.name),
-    'classes': classes,
-    'service_procedures': procedures,
-  }));
+    result.isNotEmpty ? result.last['comma'] = false : null;
+    return result;
+  }
 }
 
-Map<String, dynamic> parseProcedures(List<Procedure> procedures) {
-
-  var result = {
-    'service_procedures': [],
-    'service_getters': [],
-    'service_setters': [],
-    'class_methods': [],
-    'class_static_methods': [],
-    'class_getters': [],
-    'class_setters': [],
-  };
-
-  var serviceProcedureRE = RegExp(r'^[A-Za-z]+$');
-  var serviceGetterProcedureRE = RegExp(r'^get_([A-Za-z]+)$');
-  var serviceSetterProcedureRE = RegExp(r'^set_([A-Za-z]+)$');
-  var classMethodProcedureRE = RegExp(r'^([A-Za-z]+)_([A-Za-z]+)$');
-  var classStaticMethodProcedureRE =
-      RegExp(r'^([A-Za-z]+)_static_([A-Za-z]+)$');
-  var classGetterMethodProcedureRE = RegExp(r'^([A-Za-z]+)_get_([A-Za-z]+)$');
-  var classSetterMethodProcedureRE = RegExp(r'^([A-Za-z]+)_set_([A-Za-z]+)$');
-
-
-  procedures.forEach((procedure) {
-
-    if (serviceProcedureRE.hasMatch(procedure.name)) {
-      print('  ${procedure.name}');
-      result['service_procedures'].add({'procedure': procedure, 'name': procedure.name});
-
-    } else if (serviceGetterProcedureRE.hasMatch(procedure.name)) {
-      var match = serviceGetterProcedureRE.firstMatch(procedure.name);
-      print('  ${match.group(1)} [get]');
-      result['service_getters'].add({'procedure': procedure, 'name': match.group(1)});
-
-    } else if (serviceSetterProcedureRE.hasMatch(procedure.name)) {
-      var match = serviceSetterProcedureRE.firstMatch(procedure.name);
-      print('  ${match.group(1)} [set]');
-      result['service_setters'].add({'procedure': procedure, 'name': match.group(1)});
-
-    } else if (classMethodProcedureRE.hasMatch(procedure.name)) {
-      var match = classMethodProcedureRE.firstMatch(procedure.name);
-      print('  ${match.group(1)} - ${match.group(2)}');
-      result['class_methods']
-          .add({'class': match.group(1), 'procedure': procedure, 'name': match.group(2)});
-
-    } else if (classStaticMethodProcedureRE.hasMatch(procedure.name)) {
-      var match = classStaticMethodProcedureRE.firstMatch(procedure.name);
-      print('  ${match.group(1)} - ${match.group(2)} [static]');
-      result['class_static_methods']
-          .add({'class': match.group(1), 'procedure': procedure, 'name': match.group(2)});
-
-    } else if (classGetterMethodProcedureRE.hasMatch(procedure.name)) {
-      var match = classGetterMethodProcedureRE.firstMatch(procedure.name);
-      print('  ${match.group(1)} - ${match.group(2)} [get]');
-      result['class_getters']
-          .add({'class': match.group(1), 'procedure': procedure, 'name': match.group(2)});
-
-    } else if (classSetterMethodProcedureRE.hasMatch(procedure.name)) {
-      var match = classSetterMethodProcedureRE.firstMatch(procedure.name);
-      print('  ${match.group(1)} - ${match.group(2)} [set]');
-      result['class_setters']
-          .add({'class': match.group(1), 'procedure': procedure, 'name': match.group(2)});
-    }
-  });
-  return result;
-}
--- builder/utils/string_utils.dart
+++ builder/utils/string_utils.dart
@@ -29,23 +29,68 @@ String parseDoc(String xmlDoc) {
   var docRegexp = RegExp(r'<doc>([\S\s]+?)</doc>');
   var docMatch = docRegexp.firstMatch(xmlDoc);
   if (docMatch == null) {
-    return '/// ERROR PARSING DOCUMENTATION BLOC';
+    return '/// ERROR PARSING DOCUMENTATION BLOC\n';
   }
   var docString = docMatch.group(1);
 
+  // Start with inline stuff
+  var paramrefRegexp = RegExp(r'<paramref name="(\w+)" />');
+  docString = docString.replaceAllMapped(
+    paramrefRegexp,
+    (match) => '[${match[1]}]'
+  );
+
+  result += '/// ';
   var summaryRegexp = RegExp(r'<summary>([\S\s]+?)</summary>');
   var summaryMatch = summaryRegexp.firstMatch(docString);
   if (summaryMatch == null) {
-    return '/// ERROR PARSING DOCUMENTATION BLOC';
+    return '/// ERROR PARSING DOCUMENTATION BLOC\n';
   }
   var summaryString = summaryMatch.group(1).trim();
   var summaryStrings = summaryString.split(RegExp(r'[\n|\r|\r\n]'));
   summaryStrings.forEach((string) {
     string.trim();
-    result += '\n/// ' + string;
+    result += string + ' ';
   });
 
-  //todo: <remarks>
+  var paramRegexp = RegExp(r'<param name="(\w+)">([\S\s]+?)</param>', multiLine: true);
+  var matches = paramRegexp.allMatches(docString);
+  if (matches.isNotEmpty) {
+    matches.forEach((match) {
+      var description = match[2];
+      description = description.replaceAllMapped(RegExp(r'[\n|\r|\r\n]'), (match) => ' ');
+      result += '\n/// [${match[1]}] : ${description}';
+    });
+  }
 
-  return result;
+  var returnsRegexp = RegExp(r'<returns>([\S\s]+?)</returns>');
+  var returnsMatch = returnsRegexp.firstMatch(docString);
+  if (returnsMatch != null) {
+    result += '\n/// Returns: ';
+    var returnsString = returnsMatch.group(1).trim();
+    var returnsStrings = returnsString.split(RegExp(r'[\n|\r|\r\n]'));
+    returnsStrings.forEach((string) {
+      string.trim();
+      result += string + ' ';
+    });
+  }
+
+  var remarksRegexp = RegExp(r'<remarks>([\S\s]+?)</remarks>');
+  var remarksMatch = remarksRegexp.firstMatch(docString);
+  if (remarksMatch != null) {
+    result += '\n/// Remarks: ';
+    var remarksString = remarksMatch.group(1).trim();
+    var remarksStrings = remarksString.split(RegExp(r'[\n|\r|\r\n]'));
+    remarksStrings.forEach((string) {
+      string.trim();
+      result += string + ' ';
+    });
+  }
+
+  // todo: <see .../>
+  // todo: <list type="bullet">
+  // todo: <math>
+  // todo: <c>
+
+  return result.trim();
 }
